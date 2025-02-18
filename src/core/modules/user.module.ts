400fb637fc2ffa903c72ceb60f48b9d6a074136f
Vlad Petlyuk <vlad10112005@inbox.ru>
2025-02-18 22:55:37 +0300

    add basicly user logic

---
+++ src/core/modules/user.module.ts
@@ -0,0 +1,11 @@
+import { Global, Module } from '@nestjs/common';
+import { UserController } from '../controllers/user.controller';
+import { UserService } from '../services/user.service';
+
+@Global()
+@Module({
+  controllers: [UserController],
+  providers: [UserService],
+  exports: [UserService],
+})
+export class UserModule {}
