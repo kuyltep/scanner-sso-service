63b8fef91690e623358f18fe1189e11efa9cb8ed
Vlad Petlyuk <vlad10112005@inbox.ru>
2025-02-19 09:46:50 +0300

    add user service logic

--- src/core/modules/health.module.ts
+++ src/core/modules/health.module.ts
@@ -1,7 +1,7 @@
 import { HttpModule } from '@nestjs/axios';
 import { Module } from '@nestjs/common';
 import { TerminusModule } from '@nestjs/terminus';
-import { HealthController } from 'src/controllers/health.controller';
+import { HealthController } from '../controllers/health.controller';
 
 @Module({
   imports: [
--- src/core/services/user.service.ts
+++ src/core/services/user.service.ts
@@ -1,7 +1,60 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from './prisma.service';
+import { ExceptionService } from './exception.service';
+import { Prisma } from '@prisma/client';
+import { UserRegisterDto } from 'src/common/dtos/auth/user.register.dto';
+import { DateService } from './date.service';
 
 @Injectable()
 export class UserService {
-  constructor(private readonly prismaService: PrismaService) {}
+  constructor(
+    private readonly prismaService: PrismaService,
+    private readonly exceptionService: ExceptionService,
+    private readonly dateService: DateService,
+  ) {}
+
+  public async getUserByUnique(
+    unique: string,
+    isSelectPassword: boolean = true,
+  ) {
+    const userArgs = {
+      where: {
+        OR: [{ email: unique }, { username: unique }],
+      },
+    } as Prisma.UserFindUniqueArgs;
+    !isSelectPassword ? (userArgs.select.password = false) : null;
+    return await this.prismaService.user.findUnique(userArgs);
+  }
+
+  public async createUser(userRegisterDto: UserRegisterDto) {
+    const lastCreatedFreeTemplate =
+      await this.prismaService.subscriptionTemplate.findFirst({
+        orderBy: {
+          created_at: 'desc',
+        },
+        take: 1,
+        select: {
+          id: true,
+          limit: true,
+        },
+      });
+
+    const userCreateArgs = {
+      data: {
+        ...userRegisterDto,
+        subscription: {
+          create: {
+            end_date: this.dateService.getDateWithOffsetWithoutTime(0, -1, 0),
+            scans: lastCreatedFreeTemplate.limit,
+            template: {
+              connect: { id: lastCreatedFreeTemplate.id },
+            },
+          },
+        },
+      },
+    } as Prisma.UserCreateArgs;
+    await this.prismaService.user.create(userCreateArgs);
+
+    return { message: 'ok' };
+  }
 }
