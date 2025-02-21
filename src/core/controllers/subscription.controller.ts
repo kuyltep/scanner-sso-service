f0587ca8a949aa0f94207a18404e3ef95d78849e
Vlad Petlyuk <vlad10112005@inbox.ru>
2025-02-21 22:42:08 +0300

    update subscription logic

--- src/core/controllers/subscription.controller.ts
+++ src/core/controllers/subscription.controller.ts
@@ -1,7 +1,142 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
 import { SubscriptionService } from '../services/subscription.service';
+import {
+  ApiBody,
+  ApiExtraModels,
+  ApiOperation,
+  ApiParam,
+  ApiResponse,
+  getSchemaPath,
+} from '@nestjs/swagger';
+import {
+  GetSubscriptionDto,
+  GetSubscriptionsDto,
+} from 'src/common/dtos/subscription/get.subscription.dto';
+import { UpdateSubscriptionDto } from 'src/common/dtos/subscription/update.subscription.dto';
+import { CreateSubscriptionDto } from 'src/common/dtos/subscription/create.subscription.dto';
+import {
+  QuerySubscriptionDto,
+  QuerySubscriptionRenewDto,
+} from 'src/common/dtos/subscription/query.subscription.dto';
 
+@ApiExtraModels(
+  GetSubscriptionDto,
+  UpdateSubscriptionDto,
+  CreateSubscriptionDto,
+  GetSubscriptionsDto,
+)
 @Controller('subscriptions')
 export class SubscriptionController {
   constructor(private readonly subscriptionService: SubscriptionService) {}
+
+  @Get()
+  @ApiOperation({ summary: 'Получить все подписки' })
+  @ApiResponse({
+    status: 200,
+    description: 'Список подписок',
+    isArray: true,
+    schema: {
+      $ref: getSchemaPath(GetSubscriptionsDto),
+    },
+  })
+  async getAllSubscriptions(@Query() query: QuerySubscriptionDto) {
+    return await this.subscriptionService.getAllSubscriptions(query);
+  }
+
+  @Get(':id')
+  @ApiOperation({ summary: 'Получить подписку по ID' })
+  @ApiParam({
+    name: 'id',
+    type: String,
+    required: true,
+    description: 'ID подписки',
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'Подписка',
+    schema: {
+      $ref: getSchemaPath(GetSubscriptionDto),
+    },
+  })
+  @ApiResponse({ status: 404, description: 'Подписка не найдена' })
+  async getSubscriptionById(@Param('id') id: string) {
+    return await this.subscriptionService.getSubscriptionById(id);
+  }
+
+  @Post()
+  @ApiOperation({ summary: 'Создать новую подписку' })
+  @ApiBody({ type: CreateSubscriptionDto })
+  @ApiResponse({
+    status: 201,
+    description: 'Подписка создана',
+    schema: {
+      $ref: getSchemaPath(GetSubscriptionsDto),
+    },
+  })
+  async createSubscription(@Body() createDto: CreateSubscriptionDto) {
+    return await this.subscriptionService.createSubscription(createDto);
+  }
+
+  @Patch(':id')
+  @ApiOperation({ summary: 'Обновить подписку' })
+  @ApiParam({
+    name: 'id',
+    type: String,
+    required: true,
+    description: 'ID подписки',
+  })
+  @ApiBody({ type: UpdateSubscriptionDto })
+  @ApiResponse({
+    status: 200,
+    description: 'Подписка обновлена',
+  })
+  @ApiResponse({ status: 404, description: 'Подписка не найдена' })
+  async updateSubscription(
+    @Param('id') id: string,
+    @Body() updateDto: UpdateSubscriptionDto,
+  ) {
+    return await this.subscriptionService.updateSubscription(id, updateDto);
+  }
+
+  @HttpCode(HttpStatus.OK)
+  @Post('renew')
+  @ApiOperation({ summary: 'Продлить подписки по параметрам' })
+  @ApiResponse({
+    status: 200,
+    description: 'Подписки продлены',
+  })
+  public async renewSubscriptions(@Query() query: QuerySubscriptionRenewDto) {
+    return await this.subscriptionService.renewSubscriptions(query);
+  }
+
+  @HttpCode(HttpStatus.OK)
+  @Post(':id/renew')
+  @ApiOperation({ summary: 'Продлить подписку' })
+  @ApiParam({
+    name: 'id',
+    type: String,
+    required: true,
+    description: 'ID подписки',
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'Подписка продлена',
+    schema: {
+      $ref: getSchemaPath(GetSubscriptionsDto),
+    },
+  })
+  @ApiResponse({ status: 404, description: 'Подписка не найдена' })
+  async renewSubscription(@Param('id') id: string) {
+    return await this.subscriptionService.renewSubscription(id);
+  }
 }
--- src/core/services/subscription.service.ts
+++ src/core/services/subscription.service.ts
@@ -1,7 +1,170 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from './prisma.service';
+import { UpdateSubscriptionDto } from 'src/common/dtos/subscription/update.subscription.dto';
+import { CreateSubscriptionDto } from 'src/common/dtos/subscription/create.subscription.dto';
+import { DateService } from './date.service';
+import {
+  QuerySubscriptionDto,
+  QuerySubscriptionRenewDto,
+} from 'src/common/dtos/subscription/query.subscription.dto';
+import { Prisma } from '@prisma/client';
 
 @Injectable()
 export class SubscriptionService {
-  constructor(private readonly prismaService: PrismaService) {}
+  constructor(
+    private readonly prismaService: PrismaService,
+    private readonly dateService: DateService,
+  ) {}
+
+  public async getAllSubscriptions(query: QuerySubscriptionDto) {
+    const subscriptionsArgs = {
+      where: {
+        template: {},
+      },
+      skip: query.page_number * query.page_size,
+      take: query.page_size,
+    } as Prisma.SubscriptionFindManyArgs;
+
+    if (query.status) {
+      subscriptionsArgs.where.status = query.status;
+    }
+
+    if (query.type) {
+      subscriptionsArgs.where.template.type = query.type;
+    }
+
+    return await this.prismaService.subscription.findMany(subscriptionsArgs);
+  }
+
+  public async getSubscriptionById(id: string) {
+    return await this.prismaService.subscription.findUnique({
+      where: {
+        id,
+      },
+      include: {
+        template: true,
+      },
+    });
+  }
+
+  public async createSubscription(createDto: CreateSubscriptionDto) {
+    const { template_id, ...data } = createDto;
+    return await this.prismaService.subscription.create({
+      data: {
+        ...data,
+        end_date: data.end_date || this.dateService.getDateWithOffset(0, 1, 0),
+        template_id,
+      },
+    });
+  }
+
+  public async updateSubscription(
+    id: string,
+    updateDto: UpdateSubscriptionDto,
+  ) {
+    return await this.prismaService.subscription.update({
+      where: { id },
+      data: {
+        ...updateDto,
+      },
+    });
+  }
+
+  public async renewSubscriptions(query: QuerySubscriptionRenewDto) {
+    const subscriptionsArgs = {
+      where: {
+        template: {},
+      },
+      select: {
+        id: true,
+        template_id: true,
+      },
+    } as Prisma.SubscriptionFindManyArgs;
+    query.ids.length
+      ? (subscriptionsArgs.where.id = {
+          in: query.ids,
+        })
+      : null;
+
+    query.status ? (subscriptionsArgs.where.status = query.status) : null;
+    query.type ? (subscriptionsArgs.where.template.type = query.type) : null;
+    const subscriptions =
+      await this.prismaService.subscription.findMany(subscriptionsArgs);
+
+    const groupedByTemplate: Record<string, string[]> = {};
+    for (const subscription of subscriptions) {
+      if (!groupedByTemplate[subscription.template_id]) {
+        groupedByTemplate[subscription.template_id] = [];
+      }
+      groupedByTemplate[subscription.template_id].push(subscription.id);
+    }
+
+    const templateIds = Object.keys(groupedByTemplate);
+    const templates = await this.prismaService.subscriptionTemplate.findMany({
+      where: {
+        id: { in: templateIds },
+      },
+      select: {
+        id: true,
+        limit: true,
+      },
+    });
+
+    const templateLimits: Record<string, number> = {};
+    for (const template of templates) {
+      templateLimits[template.id] = template.limit;
+    }
+
+    const updates = [];
+    for (const templateId of templateIds) {
+      const subscriptionIds = groupedByTemplate[templateId];
+      const limit = templateLimits[templateId];
+
+      if (limit !== undefined) {
+        updates.push(
+          this.prismaService.subscription.updateMany({
+            where: {
+              id: { in: subscriptionIds },
+            },
+            data: {
+              status: 'ACTIVE',
+              scans: limit,
+              start_date: this.dateService.getCurrentDate(),
+              end_date: this.dateService.getDateWithOffset(0, 1, 0),
+            },
+          }),
+        );
+      }
+    }
+
+    await this.prismaService.$transaction(updates);
+    return { message: 'ok' };
+  }
+
+  public async renewSubscription(id: string) {
+    const template = await this.prismaService.subscriptionTemplate.findFirst({
+      where: {
+        subscriptions: {
+          some: {
+            id,
+          },
+        },
+      },
+      select: {
+        limit: true,
+      },
+    });
+
+    return await this.prismaService.subscription.update({
+      where: {
+        id,
+      },
+      data: {
+        status: 'ACTIVE',
+        scans: template.limit,
+        start_date: this.dateService.getCurrentDate(),
+        end_date: this.dateService.getDateWithOffset(0, 1, 0),
+      },
+    });
+  }
 }
